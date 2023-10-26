36ae14ffdf64b75956fe4d2eea4f8c340e228ae5
MauroRib <68522046+MauroRib@users.noreply.github.com>
2023-10-26 20:39:53 -0300

    Update loopbing.js

--- loopbing.js
+++ loopbing.js
@@ -48,7 +48,7 @@
         } else {
             comeco = 0
         };
-        var clink = "https://www.bing.com/search?q=" + links[(comeco + i)];
+        var clink = "https://www.bing.com/search?q=" + links[(comeco + i)] + "&pq=abc";
         window.open(clink, "plasma");
         i++;                    //  increment the counter
         if (i < qtd) {           //  if the counter < 10, call the loop function
